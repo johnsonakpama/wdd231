3ba566dae2d5317ca2275bc6ac2c0ac496c20f0b
johnsonakpama <johnsonakpama33@gmail.com>
2024-10-09 08:05:09 +0100

    Chamber of commerce home page completed

--- chamber/scripts/getdate.js
+++ chamber/scripts/getdate.js
@@ -12,3 +12,11 @@ full.innerHTML = `Today is <span class="highlight">${new Intl.DateTimeFormat(
 	}
 ).format(today)}</span>`;
 
+const msinnav = document.querySelector('.nav');
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
+
---
+++ chamber/scripts/windfall.js
@@ -0,0 +1,55 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector("temperature");
+const weatherIcon = document.querySelector("#weather-icon");
+const weatherType = document.querySelector("weather-type");
+const windSpeedLocation = document.querySelector("wind-speed");
+
+const url = "https://api.openweathermap.org/data/2.5/weather?id=3467684&units=metric&appid=da8fde9fff32e9d616cd34ed921f71d5"
+
+async function apiFetch() {
+  try {
+      const response = await fetch(url);
+      if (response.ok) {
+          const data = await response.json();
+          console.log(data); // this is for testing the call
+          displayResults(data);
+          displayWindChill();
+      } else {
+          throw Error(await response.text());
+      }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+apiFetch();
+
+function displayResults(weatherData) {
+  currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(1)}</strong>`;
+
+  const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+  const desc = weatherData.weather[0].description;
+  const wind = (weatherData.wind.speed * 3.6).toFixed(1);
+
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  weatherType.textContent = desc;
+  windSpeedLocation.textContent = wind;
+}
+
+function displayWindChill() {
+  // select HTML elements in the document
+  const temperature = document.querySelector(".temperature").textContent;
+  const windSpeed = document.querySelector(".wind-speed").textContent;
+  let windChill = "N/A";
+
+  // calculate wind chill
+  if (temperature <= 10 && windSpeed > 4.8) {
+      windChill = 13.12 + (0.6125 * temperature) - (11.37 * (windSpeed ** 0.16)) + (0.3965 * temperature * (windSpeed ** 0.16));
+      windChill = windChill.toFixed(1) + "\u00B0C";
+  }
+
+  // insert value
+  const windChillSpan = document.querySelector(".wind-chill");
+  windChillSpan.textContent = windChill;
+}

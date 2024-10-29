255d8514b36443aa9a85542b5bdaf05cfe4991db
johnsonakpama <johnsonakpama33@gmail.com>
2024-10-29 16:07:14 +0100

    Adjustment to my product page

--- chamber/project/scripts/product.js
+++ chamber/project/scripts/product.js
@@ -1,6 +1,6 @@
-const card = document.querySelector('#product-card');
+const card = document.querySelector('product-card');
 
-const cards  = document.querySelector('.Our-product');
+const cards  = document.querySelector('Our-product');
 
 const displayGrid = (product) => {
     const cards = document.querySelector('div.product'); // select the output container element
@@ -9,25 +9,25 @@ const displayGrid = (product) => {
         // Create elements to add to the div.cards element
         let card = document.createElement("section");
         let logo = document.createElement("img");
-        let name = document.createElement("h2");
+        let name = document.createElement("p");
         let product = document.createElement("img");
     
         // Build the image logo by setting all the relevant attribute
         logo.setAttribute("src", product.image);
-        logo.setAttribute("alt", `${name} logo`);
+        logo.setAttribute("alt", `${name} lmg`);
         logo.setAttribute("loading", "lazy");
         logo.setAttribute("width", "180");
         logo.setAttribute("height", "180");
         logo.classList.add("not-list");
 
         // Build the other content out to show the business information
-        phone.textContent = `${product.name}`;
-        website.classList.add("not-list");
-        membership.textContent = `${business.membership} Membership`;
+        products.textContent = `${product.name}`;
+        product.classList.add("not-list");
+        product.textContent = `${product.name} product`;
         membership.classList.add("not-list");
     
         // Append the section(card) with the created elements
-        card.appendChild(logo);
+        card.appendChild(img);
         card.appendChild(name);
         card.appendChild(products);
     
